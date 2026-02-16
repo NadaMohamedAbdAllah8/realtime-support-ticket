39e083a76939f3fa6e4a7f69273063c22a77022b
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2026-02-16 14:27:01 +0200

    chore: Formatting

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -20,7 +20,6 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request): JsonResponse
     {
-
         $loginDto = LoginData::from($request->validated());
 
         $payload = $this->authService->login(credentials: $loginDto);
--- app/Http/Resources/Admin/LoggedInAdminResource.php
+++ app/Http/Resources/Admin/LoggedInAdminResource.php
@@ -8,7 +8,6 @@ class LoggedInAdminResource extends JsonResource
 {
     public function toArray($request): array
     {
-
         return [
             'id' => $this->id,
             'name' => $this->name,
