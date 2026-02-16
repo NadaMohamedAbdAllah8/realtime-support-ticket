d3ab91d33011446e8f50c2e320f4d99f1ef6d932
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2026-02-16 13:10:20 +0200

    chore: Renaming user to admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $table = 'admins';
 
     /**
      * The attributes that are mass assignable.
